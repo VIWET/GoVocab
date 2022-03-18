9d625124798c79b5655600decb051b23c94560be
Dmitry Sidorov <viwet42@gmail.com>
2022-03-18 17:41:21 +0300

    TestListRepository was added

--- VocabService/app/internal/repository/teststore/store.go
+++ VocabService/app/internal/repository/teststore/store.go
@@ -3,6 +3,7 @@ package teststore
 import "github.com/VIWET/GoVocab/app/internal/domain"
 
 type store struct {
+	lists    map[int]domain.List
 	words    map[int]domain.Word
 	meanings map[int]domain.Meaning
 	usecases map[int]domain.UseCase
@@ -10,6 +11,7 @@ type store struct {
 
 func NewStore() *store {
 	return &store{
+		lists:    make(map[int]domain.List),
 		words:    make(map[int]domain.Word),
 		meanings: make(map[int]domain.Meaning),
 		usecases: make(map[int]domain.UseCase),
